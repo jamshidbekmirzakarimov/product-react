bb684aacf8dd6fed8e2b0cf204260df8706c507f
Gavharoy <125105968+Gavharoy847606@users.noreply.github.com>
2026-03-24 16:52:19 +0500

    Create Rothers
    Create Browser Router & Route & Routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import AllProducts from "./pages/AllProducts";
+import AddProduct from "./pages/AddProduct";
+import Layout from "./pages/Layout";
+import SingleProduct from "./pages/SingleProduct";
 
 export default function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/allProducts" element={<AllProducts />} />
+          <Route path="/AddProduct" element={<AddProduct />} />
+          <Route path="/SingleProduct" element={<SingleProduct />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
